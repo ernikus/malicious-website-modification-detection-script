4b8a1c9790b34f601960ca5961d0c4e10e09b7fe
lyjnamur <92183339+lyjnamur@users.noreply.github.com>
2021-10-16 14:08:28 +0200

    Update page_catch.py

--- page_catch.py
+++ page_catch.py
@@ -2,5 +2,5 @@ import urllib.request
 
 pobrany_kodstrony = urllib.request.urlopen("https://google.com")
 
-czysty_kod = data.read().decode('windows-1252') #ewentualnie utf-8, ale to bedzie wszystko tlumaczyc
+czysty_kod = pobrany_kodstrony.read().decode('windows-1252') #ewentualnie utf-8, ale to bedzie wszystko tlumaczyc
 print(czysty_kod)
