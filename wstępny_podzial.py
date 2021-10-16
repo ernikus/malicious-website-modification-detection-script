4558a774a0ae8e40344239665a9f7c466b1be1fc
lyjnamur <92183339+lyjnamur@users.noreply.github.com>
2021-10-16 15:19:05 +0200

    Add files via upload
    wstępny plik, do podziału kodu źródłowego strony na mniejsze części. 
    Do dalszej rozbudowy
    Póki co jest bardziej tylko regex żeby stwierdzić początkowy tag i końcowy tag

---
+++ wstępny_podzial.py
@@ -0,0 +1,15 @@
+import re
+
+#zrodlo_strony = ""
+#podzial_nafragmenty = re.split(/regex/ , zrodlo_strony)
+
+#print(podzial_nafragmenty)
+
+#bo jak ja chce żeby to dzieliło tekst
+
+
+#znajdowanie końca tagu
+#<\/[[:alnum:]]{1,8}>$
+
+#znajdowanie początku tagu (niezależnie od tego czy jest z atrybutami czy bez)
+#^<[[:alnum:]]{1,8}[[:ascii:]]*?>$
