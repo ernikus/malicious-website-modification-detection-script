fb2008ab2b5bcdf056264a29b52a5b1b6c366c33
ernikus <ernikus2000@gmail.com>
2021-11-28 20:59:16 +0100

    Update Structure
    Update Structure

--- page_catch.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-
-pobrany_kodstrony = urllib.request.urlopen("https://google.com")
-
-czysty_kod = pobrany_kodstrony.read().decode('windows-1252') #ewentualnie utf-8, ale to bedzie wszystko tlumaczyc
-print(czysty_kod)
