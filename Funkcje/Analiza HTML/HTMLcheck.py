39588d1a74e35f1d470a25136c5530c07e401ecd
ernikus <ernikus2000@gmail.com>
2021-11-29 18:19:54 +0100

    Added HTMLcheck
    Added HTMLcheck

---
+++ Funkcje/Analiza HTML/HTMLcheck.py
@@ -0,0 +1,95 @@
+def checkHTML():
+
+    # Open File in Read Mode 
+
+    file_1 = open('./outputHTML1.txt', 'r') 
+    file_2 = open('./outputHTML2.txt', 'r') 
+
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    import filecmp 
+
+    f1 = "/home/ernest/Documents/MegaSync/Project Python/outputHTML1.txt"
+    f2 = "/home/ernest/Documents/MegaSync/Project Python/outputHTML2.txt"
+
+
+    # shallow comparison 
+    result = filecmp.cmp(f1, f2) 
+    print(result) 
+
+    if result == False:
+        # deep comparison 
+        result = filecmp.cmp(f1, f2, shallow=False) 
+        print(result) 
+
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    if result == False:
+        print("Comparing files ", " @ " + 'file1.txt', " # " + 'file2.txt', sep='\n') 
+
+        
+        file_1_line = file_1.readline() 
+        file_2_line = file_2.readline() 
+        
+        # Use as a COunter 
+        line_no = 1
+
+        print() 
+
+        with open('./outputHTML1.txt') as file1: 
+            with open('./outputHTML2.txt') as file2: 
+                same = set(file1).intersection(file2) 
+
+        # print("Common Lines in Both Files") 
+
+        # for line in same: 
+        #     print(line, end='') 
+
+        # print('\n') 
+
+
+
+        print("Difference Lines in Both Files") 
+
+        while file_1_line != '' or file_2_line != '': 
+
+            # Removing whitespaces 
+            file_1_line = file_1_line.rstrip() 
+            file_2_line = file_2_line.rstrip() 
+
+            # Compare the lines from both file 
+            if file_1_line != file_2_line: 
+
+                # otherwise output the line on file1 and use @ sign 
+                if file_1_line == '': 
+                    print("@", "Line-%d" % line_no, file_1_line) 
+
+                else: 
+                    print("@-", "Line-%d" % line_no, file_1_line) 
+
+                # otherwise output the line on file2 and use # sign 
+                if file_2_line == '': 
+                    print("#", "Line-%d" % line_no, file_2_line) 
+
+                else: 
+                    print("#+", "Line-%d" % line_no, file_2_line) 
+
+                # Print a empty line 
+                print() 
+
+            # Read the next line from the file 
+            file_1_line = file_1.readline() 
+            file_2_line = file_2.readline() 
+
+            line_no += 1
+
+        
+        file_1.close() 
+        file_2.close() 
+        
+        
+        
+checkHTML()
