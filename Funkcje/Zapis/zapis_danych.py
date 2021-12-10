21380fbb3cb26105f489ba7975e68fd136dc0ba8
lyjnamur <92183339+lyjnamur@users.noreply.github.com>
2021-12-10 17:39:53 +0100

    Update zapis_danych.py

--- Funkcje/Zapis/zapis_danych.py
+++ Funkcje/Zapis/zapis_danych.py
@@ -2,13 +2,14 @@ from datetime import datetime
 import os
 
 def zapis(a):
+    #jeśli nie istnieje ścieżka do logów to stwórz. Powinno tylko za pierwszym razem wystąpić
     if (os.path.isdir("logs")==False):
         os.mkdir("logs")
     
     #pobranie aktualnej daty i czasu
     now = datetime.now()
     
-    #zmiana na string
+    #zmiana daty na string
     now = now.strftime("%d_%m_%Y")#_%H:%M:%S)
     #print(now)
 
@@ -16,7 +17,7 @@ def zapis(a):
     sciezka=os.getcwd();
     #print(sciezka)
 
-    #tworzenie folderu jeśli nie istnieje
+    #sprawdzenie czy istnieje lokalizacja logs, jeśli nie udało się utworzyć to powinno stworzyć raz jeszcze
     if os.path.isdir("logs"):
         os.chdir("logs")
         #x = len(list(os.listdir()))+1
@@ -30,10 +31,20 @@ def zapis(a):
         f.close()
     else:
         os.mkdir("logs")
-
-
-
+        os.chdir("logs")
+        f = open(now+".txt", "a", -1, "utf-8")
+        for i in range(0, len(a)):
+            if (type(a[i])!= str):
+                a[i] = str(a[i])
+            f.write(a[i])
+            f.write("\n")
+        f.close()
 
 
 a=['dane', 88203, "inne dane"]
 zapis(a);
+
+
+#funkcja przyjmuje pojedyńcze elementy mogące być też tablicą jedno wymiarową do zapisu
+#tworzy folder 'logs' a w nim plik z aktualną datą do którego zapisuje dane
+#każda dana w osobnej linii
