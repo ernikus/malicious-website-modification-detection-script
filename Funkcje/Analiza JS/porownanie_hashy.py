ff3a2ca7112474e0d59471e28fbd85ef3ef4391e
lyjnamur <92183339+lyjnamur@users.noreply.github.com>
2021-12-10 17:40:38 +0100

    Update porownanie_hashy.py

--- Funkcje/Analiza JS/porownanie_hashy.py
+++ Funkcje/Analiza JS/porownanie_hashy.py
@@ -1,56 +1,59 @@
 def porownanie (lista, lista2):
-    if len(lista) == len(lista2): #Sprawdzanie czy wgl nastąpiły zmiany jeśli jest ich taka sama liczba
+    if len(lista) == len(lista2): #Sprawdzanie czy nastąpiła zmiana ilości plików
         roznica_set = set(lista) - set(lista2)
         roznica = list(roznica_set)
-        print(roznica)
+#        print(roznica)  #wypisanie jaki z tych co były został zmieniony
 
-        if len(roznica) == 0:
-            print('Brak zmian w plikach .JS')
-        else:
-            print('Nastąpiła zmiana w pliku(-ach) o hashach: ', roznica)
-#zmieniajac roznica_set = set(lista2) - set(lista) mozna tez uzyskac
-#hash pliku ktory w tych nowych jest inny
+        #informacja gdy nie ma zmiany ilościowej
+        if len(roznica) != 0:
+            return('Nastąpiła zmiana w pliku(-ach) o hashach: ' ,roznica);
+            #print('Nastąpiła zmiana w pliku(-ach) o hashach: ', roznica);
+        #informacja jaki plik został zmieniony i informacja
+        #else:
+        #    print('Brak zmian w plikach .JS');    
+        #zmieniajac roznica_set = set(lista2) - set(lista) mozna tez uzyskac
+        #hash pliku ktory w tych nowych jest inny
 
-        
-    elif len(lista2) > len(lista): #Sprawdzanie kiedy jest więcej plików niż było
+    #Sprawdzanie kiedy jest więcej plików niż było
+    elif len(lista2) > len(lista): 
         lista3 = lista2[:]
-        print('Liczba plików większa niż zapisana!')
+#        print('Liczba plików większa niż zapisana!')
         
-#Określenie elementów ktore są inne
+        #Określenie elementów ktore są inne
         for x in range(len(lista)):
             try:
                 lista3.remove(lista[x])
+                #jeśli oprócz ilości jest zmiana również w pozostałych plikach
             except ValueError:
-                print('Jest więcej zmian niż tylko dodane pliki!')
-                continue
-            
-        print("Hashe plików które są inne: ", lista3)
+#                print('Jest więcej zmian niż tylko dodane pliki!')
+                continue 
+#       print("Hash(-e) plik(-u/-ów) które są inne: ", lista3)
+        return("Hash(-e) plik(-u/-ów) które zostały dodane i może zmienione: ", lista3);
 
 
-
-    else: #sprawdzenie kiedy jest mniej plikow niz było
+    #sprawdzenie kiedy jest mniej plikow niz było
+    else: 
         lista3 = lista[:]
-        print('Liczba plików mniejsza niż zapisana!')
+#        print('Liczba plików mniejsza niż zapisana!')
 
         for x in range(len(lista2)):
-            lista3.remove(lista2[x])
-        print('Hash pliku który został usunięty: ', lista3)
+            try:
+                lista3.remove(lista2[x])
+            except ValueError:
+#               print('Jest więcej zmian niż tylko dodane pliki!')
+                continue;
+        #print('Hash(-e) plik(-u/-ów) który został usunięty: ', lista3)
+        return('Hash(-e) plik(-u/-ów) które zostały usunięte i może zmienione: ', lista3);
 
 
 
 
-#lis = [15, 'like', 77, 94, 'hajs']
-#lis2 = [77, 15, 'like',94, 'hajs']
+lis = [15, 'like', 77, 94, 'hajs']
+lis2 = [77, 15, 'like',94, 'hjs', 687]
 
-#porownanie(lis, lis2)
-
-#Jeśli nie będziemy później potrzebować hashy do niczego a tylko to co zwróci funkcja czyli co się zmieniło
-#to można będzie później pousuwać lista3 i operować na lista oraz lista2
-#bo wtedy po prostu będziemy operować na oryginalnych listach a nie kopiach
+print(porownanie(lis, lis2))
 
 #Funkcja przyjmuje dwie listy hashy. Najpierw porównuje ilość plików
-#i jeśli jest taka sama jak zapisanych w logach porównuje hashe
+#i jeśli jest taka sama jak zapisanych w logach porównuje hashe (lista z logów, nowa lista)
 #jeśli natomiast liczba plików jest różna daje o tym informacje po czym
-#sprawdza pozostałe pliki w razie jakby dodatkowy plik był przynętą
-
-
+#sprawdza pozostałe pliki w razie jakby dodatkowy plik był przynętą dla przykładu
