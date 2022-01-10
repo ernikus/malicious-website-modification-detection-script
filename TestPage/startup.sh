87eb404c517188a0863a56f6c4cede9867dccbd9
ernikus <ernikus2000@gmail.com>
2022-01-10 21:02:07 +0100

    Update startup.sh

--- TestPage/startup.sh
+++ TestPage/startup.sh
@@ -7,6 +7,14 @@ echo "Changed Permission to: index.html"
 echo "Go to: http://127.0.0.1:8080/"
 
 
+sudo chmod 644 /usr/share/nginx/html/*
+sudo chmod 755 /usr
+sudo chmod 755 /usr/share
+sudo chmod 755 /usr/share/nginx
+sudo chmod 755 /usr/share/nginx/html
+echo "Changed Permission in Nginx Folders"
+
+
 pwd=$(pwd)
 sudo docker run -it --rm -d -p 8080:80 --name web -v "$(pwd)/":/usr/share/nginx/html nginx
 
