3709e2983fdfc1ef8e7164624c3b820352801fa7
lyjnamur <92183339+lyjnamur@users.noreply.github.com>
2022-01-13 23:15:26 +0100

    Add files via upload

--- FIN/bubble.py
+++ FIN/bubble.py
@@ -1,8 +1,5 @@
 # need full path to the two files
 # print diffs between these two files
-# def amountlines(f):
-#     #x=(sum(1 for line in open(f)))
-#     return (sum(1 for line in open(f)))
 
 def checkHTML(f1, f2):
 
@@ -11,7 +8,6 @@ def checkHTML(f1, f2):
     # shallow comparison
     # NOTE: here you can't write e.g."./xyz.txt"
     result = filecmp.cmp(f1, f2)
-#    print(result)
     if(result==True):
         return(result)
 
@@ -20,7 +16,6 @@ def checkHTML(f1, f2):
     if result == False:
         # deep comparison
         result = filecmp.cmp(f1, f2, shallow=False)
-#        print(result)
         if(result==True):
             return(result)
 
@@ -34,10 +29,8 @@ def checkHTML(f1, f2):
         list2 = open(f2, 'r')
         lis2 = list2.readlines()
         f = open("diffHTML.txt", "w", -1, "utf-8")
-        f1 = open("roznicaHTML.txt", "w", -1, "utf-8")
         lista = []; lista2 = []
-    #    lista = [15, 'like', 77, 94, 'hajs']
-    #    lista2 = [77, 15, 'like',94, 'hjs', 687]
+
 
         for line in lis:
             lista.append(line.strip())
@@ -45,15 +38,14 @@ def checkHTML(f1, f2):
         for line in lis2:
             lista2.append(line.strip())
 
-
+#liczba linii html jest taka sama
         if (len(lista) == len(lista2)): #Sprawdzanie czy nastąpiła zmiana ilości plików
-            roznica_set = set(lista) - set(lista2)
+            #pokazujemy co sie zmienilo w nowym pliku
+            roznica_set = set(lista2) - set(lista)
             roznica = []
             for id in roznica_set:
                 roznica.append(id)
-    #        print(len(roznica), "\n")
             for i in range(len(roznica)-1, 0 , -1):
-    #            print(i)
                 if ('google' in str(roznica[i])):
                     roznica.remove(roznica[i])
 
@@ -65,29 +57,36 @@ def checkHTML(f1, f2):
                 else:
                     continue;
 
-            #roznica = list(roznica_set)
-            #print(roznica)  #wypisanie jaki z tych co były został zmieniony
+            No=[]
+            for ele in roznica:
+                No.append(lista2.index(ele)+1)
 
-            #informacja gdy nie ma zmiany ilościowej
-#                print('Nastąpiła zmiana w lini(-ach): ' ,roznica);
+            for n in range(len(No)-1, 0, -1):
+                for i in range(n):
+                  if No[i] > No[i + 1]:
+                    # swapping data if the element is less than next element in the array
+                    No[i], No[i + 1] = No[i + 1], No[i]
+                    roznica[i], roznica[i + 1] = roznica[i + 1], roznica[i]
+
+            no=0
             if len(roznica)>0:
                 f.write('Nastąpiła zmiana w lini(-ach): \n');
                 for line in roznica:
                     line = str(line)
+                    f.write("linia ");
+                    f.write(str(No[no])); no+=1;
+                    f.write(":\t\t");
                     f.write(line);
                     f.write("\n");
-                #print('Nastąpiła zmiana w pliku(-ach) o hashach: ', roznica);
+
             #informacja jaki plik został zmieniony i informacja
             else:
                 f.write('Nie nastąpiła zadna zmiana w kodzie strony: \n');
                 return(False);
-            #zmieniajac roznica_set = set(lista2) - set(lista) mozna tez uzyskac
-            #hash pliku ktory w tych nowych jest inny
 
-        #Sprawdzanie kiedy jest więcej plików niż było
+    #Sprawdzanie kiedy jest więcej linii niż było
         elif (len(lista2) > len(lista)):
             lista3 = lista2[:]
-    #        print('Liczba plików większa niż zapisana!')
 
             #Określenie elementów ktore są inne
             for x in range(len(lista)):
@@ -95,43 +94,46 @@ def checkHTML(f1, f2):
                     lista3.remove(lista[x])
                     #jeśli oprócz ilości jest zmiana również w pozostałych plikach
                 except ValueError:
-    #                print('Jest więcej zmian niż tylko dodane pliki!')
                     continue
 
             for i in range(len(lista3)-1, 0 , -1):
-    #            print(i)
                 if ('google' in str(lista3[i])):
                     lista3.remove(lista3[i])
 
                 elif ('"datatime"' in str(lista3[i])):
-                    roznica.remove(lista3[i])
+                    lista3.remove(lista3[i])
 
                 elif ('"datetime"' in str(lista3[i])):
                     lista3.remove(lista3[i])
                 else:
                     continue;
-    #       print("Hash(-e) plik(-u/-ów) które są inne: ", lista3)
+
+            No=[]
+            for ele in lista3:
+                No.append(lista2.index(ele)+1)
+
+            no=0
             f.write("linia(-e), które zostały dodane i może zmienione: \n");
             for line in lista3:
                 line = str(line)
+                f.write("linia ");
+                f.write(str(No[no])); no+=1;
+                f.write(":\t\t");
                 f.write(line);
                 f.write("\n");
 
 
-        #sprawdzenie kiedy jest mniej plikow niz było
+        #sprawdzenie kiedy jest mniej linii niz było
         else:
             lista3 = lista[:]
-    #        print('Liczba plików mniejsza niż zapisana!')
 
             for x in range(len(lista2)):
                 try:
                     lista3.remove(lista2[x])
                 except ValueError:
-    #               print('Jest więcej zmian niż tylko dodane pliki!')
                     continue;
 
             for i in range(len(lista3)-1, 0 , -1):
-    #            print(i)
                 if ('google' in str(lista3[i])):
                     lista3.remove(lista3[i])
 
@@ -142,10 +144,18 @@ def checkHTML(f1, f2):
                     lista3.remove(lista3[i])
                 else:
                     continue;
-            #print('Hash(-e) plik(-u/-ów) który został usunięty: ', lista3)
+
+            No=[]
+            for ele in lista3:
+                No.append(lista.index(ele)+1)
+
+            no=0
             f.write('linia(-e), które zostały usunięte i może zmienione: \n');
             for line in lista3:
                 line = str(line)
+                f.write("linia ");
+                f.write(str(No[no])); no+=1;
+                f.write(":\t\t");
                 f.write(line);
                 f.write("\n");
 
@@ -156,9 +166,9 @@ def checkHTML(f1, f2):
 
 # ~ I am thinking about giving here only path to the right folder and in function specify the files to compare ~
 
-#file_no1 = "/home/kali/Desktop/projekt/FIN/logs/10_01_2022_10/HTML.txt"
-#file_no2 = "/home/kali/Desktop/projekt/FIN/logs/10_01_2022_11/HTML.txt"
-
-#checkHTML(file_no1, file_no2)
+# file_no1 = "/home/kali/Desktop/projekt/FIN/logs/10_01_2022_10/HTML.txt"
+# file_no2 = "/home/kali/Desktop/projekt/FIN/logs/10_01_2022_11/HTML.txt"
+#
+# checkHTML(file_no1, file_no2)
 
 # checkHTML()
