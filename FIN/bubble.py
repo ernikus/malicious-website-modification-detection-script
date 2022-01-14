875163ad6f77f04672788bf9bbbace085ed8a912
lyjnamur <92183339+lyjnamur@users.noreply.github.com>
2022-01-14 19:29:39 +0100

    Add files via upload

--- FIN/bubble.py
+++ FIN/bubble.py
@@ -23,14 +23,12 @@ def checkHTML(f1, f2):
 
     if result == False:
 
-
         list = open(f1, 'r')
         lis = list.readlines()
         list2 = open(f2, 'r')
         lis2 = list2.readlines()
         f = open("diffHTML.txt", "w", -1, "utf-8")
         lista = []; lista2 = []
-
 
         for line in lis:
             lista.append(line.strip())
@@ -41,37 +39,52 @@ def checkHTML(f1, f2):
 #liczba linii html jest taka sama
         if (len(lista) == len(lista2)): #Sprawdzanie czy nastąpiła zmiana ilości plików
             #pokazujemy co sie zmienilo w nowym pliku
-            roznica_set = set(lista2) - set(lista)
-            roznica = []
-            for id in roznica_set:
-                roznica.append(id)
-            for i in range(len(roznica)-1, 0 , -1):
-                if ('google' in str(roznica[i])):
-                    roznica.remove(roznica[i])
+            lista3 = lista2[:]
 
-                elif ('"datatime"' in str(roznica[i])):
-                    roznica.remove(roznica[i])
+            #Określenie elementów ktore są inne
+            for x in range(len(lista)):
+                try:
+                    lista3.remove(lista[x])
+                    #jeśli oprócz ilości jest zmiana również w pozostałych plikach
+                except ValueError:
+                    continue
 
-                elif ('"datetime"' in str(roznica[i])):
-                    roznica.remove(roznica[i])
+            for i in range(len(lista3)-1, 0 , -1):
+                if ('google' in str(lista3[i])):
+                    lista3.remove(lista3[i])
+
+                elif ('"datatime"' in str(lista3[i])):
+                    lista3.remove(lista3[i])
+
+                elif ('"datetime"' in str(lista3[i])):
+                    lista3.remove(lista3[i])
                 else:
                     continue;
 
             No=[]
-            for ele in roznica:
-                No.append(lista2.index(ele)+1)
+            pomin=[]
+            for x in range(0, len(lista3), 1):
+                for y in range(len(lista2)-1, -1, -1):
+                    if (lista3[x]==lista2[y]):# && !(y in pomin)):
+                        if((y in pomin)==False):
+                            pomin.append(y)
+                            No.append(y+1)
+                            #del lista2[y]
+                            break;
 
-            for n in range(len(No)-1, 0, -1):
-                for i in range(n):
-                  if No[i] > No[i + 1]:
-                    # swapping data if the element is less than next element in the array
-                    No[i], No[i + 1] = No[i + 1], No[i]
-                    roznica[i], roznica[i + 1] = roznica[i + 1], roznica[i]
+            if (len(No)>1):
+                for n in range(len(No)-1, 0, -1):
+                    for i in range(n):
+                      if No[i] > No[i + 1]:
+                        # swapping data if the element is less than next element in the array
+                        No[i], No[i + 1] = No[i + 1], No[i]
+                        lista3[i], lista3[i + 1] = lista3[i + 1], lista3[i]
 
             no=0
-            if len(roznica)>0:
-                f.write('Nastąpiła zmiana w lini(-ach): \n');
-                for line in roznica:
+
+            f.write('Nastąpiła zmiana w lini(-ach): \n');
+            if No:
+                for line in lista3:
                     line = str(line)
                     f.write("linia ");
                     f.write(str(No[no])); no+=1;
@@ -109,18 +122,34 @@ def checkHTML(f1, f2):
                     continue;
 
             No=[]
-            for ele in lista3:
-                No.append(lista2.index(ele)+1)
+            pomin=[]
+            for x in range(0, len(lista3), 1):
+                for y in range(len(lista2)-1, -1, -1):
+                    if (lista3[x]==lista2[y]):# && !(y in pomin)):
+                        if((y in pomin)==False):
+                            pomin.append(y)
+                            No.append(y+1)
+                            #del lista2[y]
+                            break;
+
+            if (len(No)>1):
+                for n in range(len(No)-1, 0, -1):
+                    for i in range(n):
+                      if No[i] > No[i + 1]:
+                        # swapping data if the element is less than next element in the array
+                        No[i], No[i + 1] = No[i + 1], No[i]
+                        lista3[i], lista3[i + 1] = lista3[i + 1], lista3[i]
 
             no=0
             f.write("linia(-e), które zostały dodane i może zmienione: \n");
-            for line in lista3:
-                line = str(line)
-                f.write("linia ");
-                f.write(str(No[no])); no+=1;
-                f.write(":\t\t");
-                f.write(line);
-                f.write("\n");
+            if No:
+                for line in lista3:
+                    line = str(line)
+                    f.write("linia ");
+                    f.write(str(No[no])); no+=1;
+                    f.write(":\t\t");
+                    f.write(line);
+                    f.write("\n");
 
 
         #sprawdzenie kiedy jest mniej linii niz było
@@ -146,18 +175,36 @@ def checkHTML(f1, f2):
                     continue;
 
             No=[]
-            for ele in lista3:
-                No.append(lista.index(ele)+1)
+            pomin=[]
+            for x in range(0, len(lista3), 1):
+                for y in range(len(lista)-1, -1, -1):
+                    if (lista3[x]==lista[y]):# && !(y in pomin)):
+                        if((y in pomin)==False):
+                            pomin.append(y)
+                            No.append(y+1)
+                            #del lista2[y]
+                            break;
+            print(lista3)
+            print(No)
+            if (len(No)>1):
+                for n in range(len(No)-1, 0, -1):
+                    for i in range(n):
+                      if No[i] > No[i + 1]:
+                        # swapping data if the element is less than next element in the array
+                        No[i], No[i + 1] = No[i + 1], No[i]
+                        lista3[i], lista3[i + 1] = lista3[i + 1], lista3[i]
+            print(No)
 
             no=0
             f.write('linia(-e), które zostały usunięte i może zmienione: \n');
-            for line in lista3:
-                line = str(line)
-                f.write("linia ");
-                f.write(str(No[no])); no+=1;
-                f.write(":\t\t");
-                f.write(line);
-                f.write("\n");
+            if No:
+                for line in lista3:
+                    line = str(line)
+                    f.write("linia ");
+                    f.write(str(No[no])); no+=1;
+                    f.write(":\t\t");
+                    f.write(line);
+                    f.write("\n");
 
         list.close()
         list2.close()
@@ -166,9 +213,9 @@ def checkHTML(f1, f2):
 
 # ~ I am thinking about giving here only path to the right folder and in function specify the files to compare ~
 
-# file_no1 = "/home/kali/Desktop/projekt/FIN/logs/10_01_2022_10/HTML.txt"
-# file_no2 = "/home/kali/Desktop/projekt/FIN/logs/10_01_2022_11/HTML.txt"
-#
-# checkHTML(file_no1, file_no2)
+file_no1 = "/home/kali/Desktop/project/log/14_01_2022/HTML.txt"
+file_no2 = "/home/kali/Desktop/project/log/14_01_2022_09_00_41/HTML.txt"
+
+checkHTML(file_no1, file_no2)
 
 # checkHTML()
